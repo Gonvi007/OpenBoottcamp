63614f7209c55330a1dead5d2448e4a28b2ae0e5
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-06-24 20:51:04 -0500

    Ejercicio switch

--- Listas, Objetos... Ejercicio/main.js
+++ Listas, Objetos... Ejercicio/main.js
@@ -25,3 +25,30 @@ const lista = [
         url: "https://www.amazon.com.mx/isla-misteriosa-Julio-Verne-ebook/dp/B00JF6JOX4/ref=sr_1_10?__mk_es_MX=ÅMÅŽÕÑ&crid=9LF5UD1FTTTH&keywords=Julio+verne&qid=1656121402&sprefix=julio%2Caps%2C1207&sr=8-10"
       },
   ];
+
+
+//   Sentencia Switch
+
+let nota = 5;
+
+switch (nota) {
+    case 5:
+        console.log("Buen trabajo, ¡Sobresaliente!")
+        break;
+    case 4:
+        console.log("Buen trabajo, ¡Sobresaliente!")
+        break;
+    case 3:
+        console.log("Suficiente pero no bien")
+        break;
+    case 2:
+        console.log("Valiste verga pa")
+        break;
+    case 1:
+        console.log("A chinga, venias?")
+        break;
+    default:
+        console.log("Error");
+        break;
+
+}
